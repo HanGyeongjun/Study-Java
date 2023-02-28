e5ee42a12992463b71bf3770b87b6813e57125fb
HanGyeongjun <030212hgjun@gmail.com>
2023-02-28 23:51:35 +0900

    Chapter_01 / HelloWorld and Types

---
+++ Chapter_01/_02_DataTypes.java
@@ -0,0 +1,14 @@
+package Chapter_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("Hello, World"); //String
+        System.out.println(12); //Numeric
+        System.out.println(-34);
+        System.out.println(3.14);
+        System.out.println(true); //Boolean
+        System.out.println(false);
+
+        System.out.println(222 - 22);
+    }
+}
